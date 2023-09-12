4082d16bb5f966961e845e7357c294c25334cddf
Jacky Ho <hojacky@microsoft.com>
2023-09-12 13:20:17 -0400

    removed and replaced pages

---
+++ src/routes/pantry/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Pantry</title>
+	<meta name="description" content="Pantry" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Pantry</h1>
+</div>
---
+++ src/routes/recipes/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Recipes</title>
+	<meta name="description" content="Recipes" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Recipes</h1>
+</div>
